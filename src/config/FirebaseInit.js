f45fcfb39a47b0fcbd3cdfb8ddad90eb04c909e9
Otto Spreng <otto.spreng@sou.unifeob.edu.br>
2020-05-22 10:49:04 -0300

    Revert "fixing .env.example"
    This reverts commit 27a9c2e5ca20bd0136a02a92e17f5e8701e4d48f.

--- src/config/FirebaseInit.js
+++ src/config/FirebaseInit.js
@@ -1,15 +1,11 @@
 import * as admin from "firebase-admin";
-import dotenv from "dotenv";
-
 import serviceAccount from "./ServiceAccountKey.json";
-
-dotenv.config();
 
 class FirebaseInit {
   static init() {
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
-      databaseURL: process.env.DB_URL,
+      databaseURL: "https://petsaojoao-86487.firebaseio.com",
     });
   }
 }
